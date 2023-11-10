5e10214c3c0dab0acf18cc5b362a0edd5f919007
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-11-10 18:16:11 +0600

    [Done] booking sysytem completed

--- petbnb-backend-9/src/app/modules/booking/booking.controller.ts
+++ petbnb-backend-9/src/app/modules/booking/booking.controller.ts
@@ -2,7 +2,9 @@ import { Booking } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { BookingFilterableFields } from './booking.constant';
 import { BookingService } from './booking.service';
 
 const createBooking = catchAsync(async (req: Request, res: Response) => {
@@ -45,8 +47,79 @@ const startBooking = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const finishBooking = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BookingService.startBooking(id);
+
+  sendResponse<Booking>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Booking is Finished',
+    data: result,
+  });
+});
+
+const getAllBooking = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, BookingFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await BookingService.getAllBooking(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Booking all retrives successfully',
+
+    data: result,
+  });
+});
+
+const getBooking = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookingService.getBooking(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Booking retrives successfully',
+    data: result,
+  });
+});
+
+const updateBooking = catchAsync(async (req: Request, res: Response) => {
+  const { ...data } = req.body;
+
+  const { id } = req.body;
+  const result = await BookingService.updateBooking(id, data);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Booking Updated successfully',
+
+    data: result,
+  });
+});
+
+const deleteBooking = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.body;
+
+  const result = await BookingService.deleteBooking(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Booking Delete successfully',
+
+    data: result,
+  });
+});
 export const BookingController = {
   createBooking,
   cancelBooking,
   startBooking,
+  finishBooking,
+  getAllBooking,
+  getBooking,
+  updateBooking,
+  deleteBooking,
 };
--- petbnb-backend-9/src/app/modules/booking/booking.routes.ts
+++ petbnb-backend-9/src/app/modules/booking/booking.routes.ts
@@ -3,8 +3,13 @@ import { BookingController } from './booking.controller';
 
 const router = express.Router();
 
-router.post('/create', BookingController.createBooking);
-router.patch('/cancel-booking/:id', BookingController.cancelBooking);
-router.patch('/:id', BookingController.startBooking);
+router.post('/book-appointment', BookingController.createBooking);
+router.patch('/cancel-appointment/:id', BookingController.cancelBooking);
+router.patch('/start-appointment/:id', BookingController.startBooking);
+router.patch('/finish-appointment/:id', BookingController.finishBooking);
+router.get('/', BookingController.getAllBooking);
+router.get('/:id', BookingController.getBooking);
+router.patch('/:id', BookingController.updateBooking);
+router.delete('/:id', BookingController.deleteBooking);
 
 export const BookingRouter = router;
